9b12785d4dbf7d0675e4c66a76ab81900459e6af
Kosmický Petr <petr.pospichal09@gmail.com>
2026-05-29 20:59:42 +0200

    serializováno

--- src/Model/Category.java
+++ src/Model/Category.java
@@ -1,6 +1,8 @@
 package Model;
 
-public class Category {
+import java.io.Serializable;
+
+public class Category implements Serializable {
 
     private String name;
     private String description;
