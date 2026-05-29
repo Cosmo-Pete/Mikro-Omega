0619f2ff73277b45bc5202571598357e32d5dc00
Kosmický Petr <petr.pospichal09@gmail.com>
2026-05-29 21:47:00 +0200

    Implementace metod v MultipleChoiceQuestion

--- src/Model/MultipleChoiceQuestion.java
+++ src/Model/MultipleChoiceQuestion.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 public class MultipleChoiceQuestion extends Question {
 
-    private List<String> options;       // seznam možností A, B, C, D
-    private int correctIndex;           // index správné odpovědi
+    private List<String> options;       // list of answer options
+    private int correctIndex;           // index of the correct answer
 
     public MultipleChoiceQuestion(String questionText, String difficulty,
-                                   Category category, int timeLimit,
-                                   List<String> options, int correctIndex) {
+                                  Category category, int timeLimit,
+                                  List<String> options, int correctIndex) {
         super(questionText, difficulty, category, timeLimit);
         this.options = options;
         this.correctIndex = correctIndex;
@@ -24,19 +24,38 @@ public class MultipleChoiceQuestion extends Question {
         return correctIndex;
     }
 
-    // Implementace z Answerable
+    /**
+     * Checks if the given answer index matches the correct answer index.
+     * @param answer the index of the selected option
+     * @return true if correct, false otherwise
+     */
     @Override
     public boolean checkAnswer(Object answer) {
+        if (answer instanceof Integer) {
+            return (Integer) answer == correctIndex;
+        }
         return false;
     }
 
+    /**
+     * Returns the correct answer index.
+     * @return correct index
+     */
     @Override
     public Object getCorrectAnswer() {
-        return null;
+        return correctIndex;
     }
 
+    /**
+     * Returns the formatted question with all options.
+     * @return formatted question string
+     */
     @Override
     public String getFormattedQuestion() {
-        return "";
+        String result = getQuestionText() + "\n";
+        for (int i = 0; i < options.size(); i++) {
+            result += (char) ('A' + i) + ") " + options.get(i) + "\n";
+        }
+        return result;
     }
 }
